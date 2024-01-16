23a4fb5aec0b3c16bc8b7fbff3dda97fe9613b96
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-01-17 01:22:10 +0200

    Change author to user as advised by code reviewer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :user, class_name: 'User', foreign_key: :author_id
 
   after_save :update_post_comments_counter
 
