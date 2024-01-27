c528741b337f37d9ec97727cd5dd3991887b223b
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-01-28 01:21:06 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "users#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   get "/users", to: "users#index"
-  get "/users/:user_id", to: "users#show", as: "user"
+
+  get "/users/:id", to: "users#show", as: "user"
+
+  get "/users/:user_id/posts/new", to: "posts#new", as: "new_user_post"
+  post "/users/:user_id/posts", to: "posts#create", as: "posts"
+
+  get "/users/:user_id/posts/:post_id/comments/new", to: "comments#new", as: "new_user_post_comment"
+  post "/users/:user_id/posts/:post_id/comments", to: "comments#create", as: "user_post_comments"
+
+  post "/users/:user_id/posts/:post_id/likes", to: "likes#create", as: "user_post_likes"
+
   get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
-  get "/users/:user_id/posts/:post_id", to: "posts#show", as: "user_post"
+
+  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
 end
