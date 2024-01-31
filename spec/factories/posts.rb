54863ffd618682bcc1065d71019164c408cdf5bf
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-01-31 18:26:43 +0200

    Update posts

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
   factory :post do
     # Define the attributes for the Post model
     # Example:
-    title { 'Sample Title' }
+    title { "Sample Title" }
+    text { "sample text" }
     liked_counter { 5 }
     comments_counter { 1 }
     # You can associate the post with the user by defining the author association
