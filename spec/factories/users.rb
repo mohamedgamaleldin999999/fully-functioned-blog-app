0dbcfbc778ce0339ab18e1fcd4ea5c679ca16759
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-01-31 18:27:55 +0200

    Update users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
   factory :user do
     # Define the attributes for the User model
     # Example:
-    name { 'Ahmed Eid' }
-    photo { 'https://unsplash.com/photos/F_-0BxGuVvo' }
-    bio { 'Backend developer from Egypt' }
+    name { "Ahmed Eid" }
+    photo { "https://unsplash.com/photos/F_-0BxGuVvo" }
+    bio { "Backend developer from Egypt" }
   end
 end
