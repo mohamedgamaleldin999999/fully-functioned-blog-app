144e93dc55cdaaa9771d99051c562a636910d560
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-02-03 14:38:59 +0200

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,13 @@
 class Ability
   include CanCan::Ability
- 
-      def initialize(user)
- 
-          user ||= User.new # guest user (not logged in)
- 
-          if user.admin? 
-          can :manage, :all
-          else
-          can :manage, Post, author: user  # owner of post can perform all view, update or destroy own post if Post.author == user
-          can :manage, Comment, author: user
-          can :read, :all
-          end
+    def initialize(user)
+      user ||= User.new # guest user (not logged in)
+      if user.admin? 
+      can :manage, :all
+      else
+      can :manage, Post, author: user  # owner of post can perform all view, update or destroy own post if Post.author == user
+      can :manage, Comment, author: user
+      can :read, :all
       end
   end
+end
