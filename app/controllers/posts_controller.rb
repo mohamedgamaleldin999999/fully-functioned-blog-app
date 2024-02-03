2bc129268969c65c4e42764d1032bd6d5a8c8abf
Mark Rubanza <markrubanza10@gmail.com>
2024-02-03 13:50:38 +0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,35 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
-
   def index
     @user = User.includes(:posts, :comments).find(params[:user_id])
     @posts = @user.posts
   end
-
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
-
   def new
     @post = Post.new
   end
-
   def create
     @post = current_user.posts.new(post_params.merge(liked_counter: 0, comments_counter: 0))
     if @post.save
       redirect_to user_post_path(@post.author, @post)
     else
-      flash.now[:alert] = "Post creation failed!"
+      flash.now[:alert] = 'Post creation failed!'
       render :new
     end
   end
-
   def destroy
     if @post.destroy
-      redirect_to user_posts_path(@post.author), notice: "Post deleted successfully."
+      redirect_to user_posts_path(@post.author), notice: 'Post deleted successfully.'
     else
-      flash[:alert] = "You are not authorized to delete that post."
+      flash[:alert] = 'You are not authorized to delete that post.'
       render :show
     end
   end
-
   private
-
   def post_params
     params.require(:post).permit(:title, :text, :liked_counter, :comments_counter)
   end
