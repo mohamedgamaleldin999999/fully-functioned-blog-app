01ae037db8a95e8161b354f20ea31969e6f1c21e
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2024-02-03 14:36:29 +0200

    Update ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,16 @@
 class Ability
   include CanCan::Ability
-
-  def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.role == 'admin'
-      can :manage, Post
-      can :manage, Comment
-    else
-      can :destroy, Post, author_id: user.id
-      can :destroy, Comment, author_id: user.id
-    end
+ 
+      def initialize(user)
+ 
+          user ||= User.new # guest user (not logged in)
+ 
+          if user.admin? 
+          can :manage, :all
+          else
+          can :manage, Post, author: user  # owner of post can perform all view, update or destroy own post if Post.author == user
+          can :manage, Comment, author: user
+          can :read, :all
+          end
+      end
   end
-end
